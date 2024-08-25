8205ec91e0711b5d45c81e9177a3c7d9d68a8dd3
denissn228 <odxmbpbkrw@rambler.ru>
2024-08-25 12:32:22 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("Toops","top")
+        ERC721("qwer","qw")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
